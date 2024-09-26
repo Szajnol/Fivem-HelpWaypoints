73595c156ef36be87a3738e8bbccd45c48d05f9e
Dawid <118669019+Szajnol@users.noreply.github.com>
2024-09-26 19:19:59 +0200

    Add files via upload

---
+++ shinyx-HelpWaypoints/client.lua
@@ -0,0 +1,16 @@
+exports('DrawDestination', function (coords, label, meters)
+    DrawDestination(coords, label, meters)
+end)
+
+function DrawDestination(coords, label, meters)
+    local X, Y = GetScreenCoordFromWorldCoord(coords.x, coords.y, coords.z)
+    local icon_size = 1.5
+    local text_size = 0.25
+    RequestStreamedTextureDict("basejumping", false)
+    DrawSprite("texture", "e-key", X, Y - 0.015, 0.018 * icon_size, 0.025 * icon_size, 0.0, 255, 255, 255, 255)
+    SetTextCentre(true)
+    SetTextScale(0.0, text_size)
+    SetTextEntry("STRING")
+    AddTextComponentString(label .. "\n".. meters .. "m")
+    DrawText(X, Y + 0.015)
+end
---
+++ shinyx-HelpWaypoints/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+version '1.0.0'
+
+client_scripts {
+	'client.lua',
+}
+
+lua54 'yes'
